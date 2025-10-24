fa07e8f8b716a78231c2f8ee847b89380798e8a8
pablo-fernandez-IGZ <pablo.fernandezl@intelygenz.com>
2025-10-24 23:51:41 +0200

    make main for ingesting

--- code/ingest.py
+++ code/ingest.py
@@ -6,6 +6,7 @@ from langchain_chroma import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from PIL import Image
+from pathlib import Path
 
 load_dotenv()
 
@@ -13,8 +14,8 @@ def image_to_text(image: Image.Image) -> str:
     #TODO: integrate a vision model
     return "Extracted text from image"
 
-def load_document(file_path: str) -> pypdf.PdfReader:
-    if not file_path.endswith('.pdf'):
+def load_document(file_path: Path) -> pypdf.PdfReader:
+    if not file_path.suffix == '.pdf':
         raise ValueError("Unsupported file format")
     document = pypdf.PdfReader(file_path)
     return document
@@ -39,22 +40,26 @@ def document_to_text(document: pypdf.PdfReader) -> str:
             
     return text
 
+def ingest_docs(docs_path:str):
+    docs_path = Path(docs_path)
+    for file_path in docs_path.iterdir():
+        document = load_document(file_path)
+        
+        text = document_to_text(document)
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,  # chunk size (characters)
+            chunk_overlap=200,  # chunk overlap (characters)
+            add_start_index=True,  # track index in original document
+        )
+        chunks = text_splitter.create_documents(text)
+        embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+        vector_store = Chroma(
+            collection_name="example_collection",
+            embedding_function=embeddings,
+            persist_directory="./chroma_langchain_db",
+        )
+        document_ids = vector_store.add_documents(documents=chunks)
+        return document_ids
 
 if __name__ == "__main__":
-    file_path = "docs/sample.pdf"
-    document = load_document(file_path)
-      
-    text = document_to_text(document)
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,  # chunk size (characters)
-        chunk_overlap=200,  # chunk overlap (characters)
-        add_start_index=True,  # track index in original document
-    )
-    chunks = text_splitter.create_documents(text)
-    embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-    vector_store = Chroma(
-        collection_name="example_collection",
-        embedding_function=embeddings,
-        persist_directory="./chroma_langchain_db",
-    )
-    document_ids = vector_store.add_documents(documents=chunks)
+    ingest_docs("docs/")
