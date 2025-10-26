265af6147497679deffd6d50a136bb6fbbcc4106
pablo-fernandez-IGZ <pablo.fernandezl@intelygenz.com>
2025-10-26 16:13:27 +0100

    tests done

--- code/agent_test.py
+++ code/agent_test.py
@@ -1,6 +1,17 @@
 import pytest
 from langchain.messages import AIMessage
-from agent import is_chat_too_long, State
+from agent import is_chat_too_long, State, init_vector_store, create_initial_state, retrieve, is_tool_call
+from langgraph.graph import END
+
+
+def test_retrieve_returns_n_documents():
+    vector_store = init_vector_store()
+    state = create_initial_state(vector_store)
+    state.num_retrieved_documents = 3
+    state.query = "sample query"
+    state = retrieve(state)
+    assert len(state.context) == 3
+
 
 @pytest.mark.parametrize(
     "max_tokens, current_tokens, expected",
@@ -18,3 +29,34 @@ def test_is_chat_too_long(mocker, max_tokens, current_tokens, expected):
     response = is_chat_too_long(state)
 
     assert response == expected
+
+
+def test_is_tool_call(mocker):
+    mock_tool_response = mocker.Mock()
+    mock_tool_response.content = "Tool executed successfully"
+    vector_store = init_vector_store()
+    state = create_initial_state(vector_store)
+    state.chat_history.append(
+        AIMessage(
+            content="Executing tool",
+            tool_calls=[
+                {"name": "execute_python_code", "args": {"code": "print('Hello World')", "id": "1"}, "id": "1"}
+            ],
+        )
+    )
+
+    routing = is_tool_call(state)
+
+    assert routing == "execute_tool"
+
+
+def test_is_not_tool_call(mocker):
+    mock_tool_response = mocker.Mock()
+    mock_tool_response.content = "Tool executed successfully"
+    vector_store = init_vector_store()
+    state = create_initial_state(vector_store)
+    state.chat_history.append(AIMessage(content="Not Executing tool"))
+
+    routing = is_tool_call(state)
+
+    assert routing == END
